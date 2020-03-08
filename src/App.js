a66e272541c71b48bbde385c5d095d57f7ea3b58
Fareez Ahamed <fareez.ahamed@gmail.com>
2020-03-08 17:03:25 +0530

    Completed the sample application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-screen-md mx-auto">
+      <div
+        style={{
+          backgroundColor: "#cccccc",
+          height: "100rem"
+        }}
+      ></div>
+      <StarWarsTable> </StarWarsTable>
     </div>
   );
 }
 
+const StarWarsTable = props => {
+  const [chars, setChars] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const ref = React.createRef();
+
+  useScreenEnter(ref, () => {
+    // Show loading
+    setLoading(true);
+
+    // Fetch data
+    fetch("https://swapi.co/api/people?format=json")
+      .then(res => res.json())
+      .then(data => {
+        setChars(data.results);
+        setLoading(false);
+      });
+  });
+
+  return (
+    <div ref={ref}>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table className="">
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Gender</td>
+              <td>Height</td>
+              <td>Mass</td>
+            </tr>
+          </thead>
+          <tbody>
+            {chars.map(person => (
+              <tr key={person.name}>
+                <td>{person.name}</td>
+                <td>{person.gender}</td>
+                <td>{person.height}</td>
+                <td>{person.mass}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 export default App;
+
+export function useScreenEnter(ref, callback) {
+  const [entered, setEntered] = useState(false);
+
+  function activate() {
+    if (
+      ref.current &&
+      isInViewPort(ref.current.getBoundingClientRect()) &&
+      !entered
+    ) {
+      callback();
+      setEntered(true);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", activate);
+    return () => document.removeEventListener("scroll", activate);
+  });
+}
+
+function isInViewPort(rect) {
+  if (
+    window.screen.height >= rect.bottom &&
+    window.screen.width >= rect.right &&
+    rect.top >= 0 &&
+    rect.left >= 0
+  )
+    return true;
+  return false;
+}
